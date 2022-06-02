0fe1b3328c355a122fd28275b60a2e4782db6002
ishidao0910 <goo.ishidao@gmail.com>
2022-06-02 09:57:03 +0900

    update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import datetime
 
 if __name__ == '__main__':
 	dt_now = datetime.datetime.now()
-	add_txt = '<br>' + str(dt_now) + '\n'
+	add_txt = str(dt_now) + '\n'
 	print(dt_now)
 
-	file_path = "README.md" 
+	file_path = "data/every_hour.txt" 
 	with open(file_path, "r") as file:
 		txt = file.read()
 		txt = txt + add_txt
